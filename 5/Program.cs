e1d2a18f3a23bc42a5fd94cdbe1b737a2fd3e89c
IsaelGabriel <isaelgsfaria@gmail.com>
2024-09-03 02:44:53 -0300

    Add 5 as C# project

---
+++ 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+static class Program {
+    const string BaseString = "O rato roeu a roupa do rei de Roma!";
+
+    static void Main(string[] args) {
+        StringBuilder stringBulder = new StringBuilder(BaseString);
+        int left = 0;
+        int right = BaseString.Count() - 1;
+        char sub;
+
+        while(left < right) {
+            sub = stringBulder[left];
+            stringBulder[left] = stringBulder[right];
+            stringBulder[right] = sub;
+            left++;
+            right--;
+        }
+
+        Console.WriteLine(stringBulder.ToString());
+    }
+}
